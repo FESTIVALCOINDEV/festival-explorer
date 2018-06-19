0297be86fd76f5e9ee443a8f3b88b1eccdbbdf93
FESTIVALCOINDEV <36564763+FESTIVALCOINDEV@users.noreply.github.com>
2018-06-19 16:32:25 +0200

    Add files via upload

---
+++ festival_explorer/config.js
@@ -0,0 +1,11 @@
+var api = 'http://206.189.115.142:8349';
+var blockTargetInterval = 120;
+var coinUnits = 100000000;
+var symbol = 'FEST';
+var refreshDelay = 30000;
+var networkStat = {
+    'fest': [
+       ['festival.multiminer.us', "https://festival.multiminer.us:8119"],
+       ['youpool.io/FEST', "https://youpool.io:8116"]
+    ]
+};
